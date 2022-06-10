55b09eb71b8aa49eff93efa75fb009b996f66af8
cauabruno <106626317+cauabruno@users.noreply.github.com>
2022-06-10 16:05:40 -0300

    Comitando de novo

---
+++ projeto2.js
@@ -0,0 +1,15 @@
+function clicar()
+{
+    var x = document.getElementById("q1")
+    
+    if (x.style.background == "skyblue")
+    {
+        x.style.background = "red"
+    }
+    
+    else
+    {
+        x.style.background = "skyblue"
+    }
+
+}
