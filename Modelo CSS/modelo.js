fac0ff7f8fd9aeccb339731adb215395fc8821ba
cauabruno <106626317+cauabruno@users.noreply.github.com>
2022-06-11 22:49:53 -0300

    Criando o modelo

---
+++ Modelo CSS/modelo.js
@@ -0,0 +1,12 @@
+function clicar()
+{
+    var cor = document.getElementById("q1")
+    if (cor.style.background == "skyblue")
+    {
+        cor.style.background = "red";
+    }
+    else
+    {
+        cor.style.background = "skyblue"
+    }
+}
